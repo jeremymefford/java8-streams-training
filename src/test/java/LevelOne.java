09a52c133129be058978c2961c3e0d5b524a1649
Jeremy Mefford <jeremy.mefford@us.usana.com>
2016-01-06 09:35:20 -0700

    solutions

---
+++ src/test/java/LevelOne.java
@@ -0,0 +1,223 @@
+import com.google.common.collect.Sets;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Collections and transformations
+ *
+ * @author Jeremy Mefford
+ * @since 1/5/16
+ */
+public class LevelOne {
+
+    final Random usedInNumberEleven = new Random(123456L);
+
+    @Test
+    public void testOne() {
+        // given
+        List<Integer> start = Arrays.asList(1, 2, 3, 4, 5);
+
+        // when i square every element in the list
+//        List<Integer> end = null;
+        List<Integer> end = start.stream()
+                .map(i -> i * i)
+                .collect(Collectors.toList());
+
+        // then
+        Assert.assertEquals(Arrays.asList(1, 4, 9, 16, 25), end);
+    }
+
+    @Test
+    public void testTwo() {
+        // given
+        List<Integer> start = Arrays.asList(1, 2, 3, 4, 5);
+
+        // when i take the log base 2 of each number
+//        List<Double> end = null;
+        List<Double> end = start.stream()
+                .map(Math::log)
+                .collect(Collectors.toList());
+
+        // then
+        Assert.assertEquals(Arrays.asList(0.0, 0.6931471805599453, 1.0986122886681098, 1.3862943611198906, 1.6094379124341003), end);
+    }
+
+    @Test
+    public void testThree() {
+        // given
+        List<String> start = Arrays.asList("asdf", "jkl;", "qwerty", null, "foobar");
+
+        // when i transform each string to upper case
+//        List<String> end = null;
+        List<String> end = start.stream()
+                .filter(Objects::nonNull)
+                .map(String::toUpperCase)
+                .collect(Collectors.toList());
+
+
+        // then
+        Assert.assertEquals(Arrays.asList("ASDF", "JKL;", "QWERTY", "FOOBAR"), end);
+    }
+
+    @Test
+    public void testFour() {
+        // given
+        List<String> start = Arrays.asList("asdf", "asdf", "asdf", "asdf");
+
+        // when i deduplicate this list
+//        Set<String> end = null;
+        Set<String> end = start.stream()
+                .collect(Collectors.toSet());
+
+        // then
+        Assert.assertEquals(Sets.newHashSet("asdf"), end);
+    }
+
+    @Test
+    public void testFive() {
+        // given
+        List<String> start = Arrays.asList("asdf", "asdf", "asdf", "asdf");
+
+        // when i deduplicate this list
+//        LinkedList<String> end = null;
+        LinkedList<String> end = start.stream()
+                .distinct()
+                .collect(Collectors.toCollection(LinkedList<String>::new));
+
+        // then
+        Assert.assertEquals(new LinkedList<>(Collections.singleton("asdf")), end);
+    }
+
+    @Test
+    public void testSix() {
+        // given
+        List<String> start = Arrays.asList("a", "ab", "abc", "abcd");
+
+        // when i map the string to it's length
+//        Map<String, Integer> end = null;
+        Map<String, Integer> end = start.stream()
+                .collect(Collectors.toMap(
+                        Function.identity(),
+                        String::length
+                ));
+
+        // then
+        Assert.assertEquals(new HashMap<String, Integer>() {{
+            put("a", 1);
+            put("ab", 2);
+            put("abc", 3);
+            put("abcd", 4);
+        }}, end);
+    }
+
+    @Test
+    public void testSeven() {
+        // given
+        List<Integer> start = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+
+        // when i find all of the even numbers
+//        List<Integer> end = null;
+        List<Integer> end = start.stream()
+                .filter(i -> i % 2 == 0)
+                .collect(Collectors.toList());
+
+        // then
+        Assert.assertEquals(Arrays.asList(2, 4, 6), end);
+    }
+
+    private String repeatBySoManyNumbers(int i) {
+        StringBuilder ret = new StringBuilder();
+        for (int x = 0; x < i; x++) {
+            ret.append(String.valueOf(i));
+        }
+        return ret.toString();
+    }
+
+    @Test
+    public void testEight() {
+        // given
+        List<Integer> start = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        // when i find all of the even numbers
+        // and create strings containing that number repeated by it's value (eg, 2 -> "22", 4 -> "4444")
+        // hint. use the repeatBySoManyNumbers() method
+//        List<Integer> end = null;
+        List<String> end = start.stream()
+                .filter(i -> i % 2 == 0)
+                .map(this::repeatBySoManyNumbers)
+                .collect(Collectors.toList());
+
+        // then
+        Assert.assertEquals(Arrays.asList("22", "4444", "666666", "88888888", "10101010101010101010"), end);
+    }
+
+
+    @Test
+    public void testNine() {
+        // given
+        List<Integer> start = Arrays.asList(1, 2, 3, 4, 5);
+        List<String> dest = new ArrayList<>();
+
+        // when i move odd numbers from start to dest (an existing collection) using streams
+        // the numbers should be a string in dest (eg, 1 -> "1")
+        // do not use an intermediate collection (no calls to .collect())
+        start.stream()
+                .filter(i -> i % 2 == 1)
+                .map(String::valueOf)
+                .forEach(dest::add);
+
+        // then
+        Assert.assertEquals(Arrays.asList("1", "3", "5"), dest);
+    }
+
+    private class TestTenHelper {
+        private String usefulMethod(int i) {
+            return "echo: " + i;
+        }
+    }
+
+    @Test
+    public void testTen() {
+        // given
+        List<Integer> start = Arrays.asList(1, 2);
+        TestTenHelper helper = new TestTenHelper();
+
+        // when i use helper to collect items in start
+        // hint. should use a method reference here
+//        List<String> end = null;
+        List<String> end = start.stream()
+                .map(helper::usefulMethod)
+                .collect(Collectors.toList());
+
+        // then
+        Assert.assertEquals(Arrays.asList("echo: 1", "echo: 2"), end);
+    }
+
+    public Long generatorSupplier() {
+        long curr = usedInNumberEleven.nextLong();
+        return curr++ % 3L == 0 ? null : curr;
+    }
+
+    @Test
+    public void testEleven() {
+        // given
+        Stream<Long> nullGeneratingStream = Stream.generate(this::generatorSupplier);
+
+        // find how many null elements there are in the first 1000 elements
+        // ex. credit. log every element to system out
+//        long count = -1L;
+        long count = nullGeneratingStream
+                .peek(System.out::println)
+                .limit(1000)
+                .filter(Objects::isNull)
+                .count();
+
+        // then
+        Assert.assertEquals(364, count);
+    }
+}
---
+++ src/test/java/LevelTwo.java
@@ -0,0 +1,121 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.*;
+import java.util.stream.Stream;
+
+/**
+ * Searching, optionals and reduction
+ *
+ * @author Jeremy Mefford
+ * @since 1/5/16
+ */
+public class LevelTwo {
+
+    public static boolean hasWholeRoot(int i) {
+        double root = Math.sqrt(i);
+        return (int) root == root;
+    }
+
+    @Test
+    public void testOne() {
+        // given
+        List<Integer> start = Arrays.asList(2, 5, 12, 16, 23, 31);
+
+        System.out.println("========= Starting Test One =========");
+        // find the first number in the list that has a whole square root (eg. 9 has a whole root of 3)
+        // bonus, log the numbers as the stream processes and see which elements are actually traversed.  is it all of them?
+//        Optional<Integer> hasWholeRoot = null;
+        Optional<Integer> hasWholeRoot = start.stream()
+                .peek(System.out::println)
+                .filter(LevelTwo::hasWholeRoot)
+                .findFirst();
+
+        // then
+        Assert.assertEquals(Optional.of(16), hasWholeRoot);
+    }
+
+    @Test
+    public void testTwo() {
+        // given
+        List<Integer> start = Arrays.asList(3, 5, 13, 17, 23, 31);
+
+        // find the first even number (no, you cannot modify the original sequence)
+        // return it as the string version of itself, or null if not present
+//        Optional<String> end = null;
+        Optional<String> end = start.stream()
+                .filter(i -> i % 2 == 0)
+                .findFirst()
+                .map(String::valueOf);
+
+        // then
+        Assert.assertEquals(Optional.ofNullable(null), end);
+    }
+
+    @Test
+    public void testThree() {
+        // given
+        List<BigDecimal> start = Arrays.asList(
+                new BigDecimal("1"),
+                new BigDecimal("1.12345612341212351"),
+                new BigDecimal("10.142"),
+                new BigDecimal("-1.2"),
+                new BigDecimal("100.56789")
+        );
+
+        // when i sum them all together
+//        BigDecimal end = null;
+        BigDecimal end = start.stream()
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        // then
+        Assert.assertEquals(new BigDecimal("111.63334612341212351"), end);
+    }
+
+    @Test
+    public void testFour() {
+        // given
+        Stream<BigDecimal> start = Stream.of(
+                new BigDecimal("1"),
+                new BigDecimal("1.12345612341212351"),
+                new BigDecimal("10.142"),
+                new BigDecimal("-1.2"),
+                new BigDecimal("100.56789"),
+                null
+        );
+
+        // when i get the last non-null element in the stream
+        // hint. use reduce
+//        BigDecimal end = null;
+        BigDecimal end = start
+                .filter(Objects::nonNull)
+                .reduce(null, (x1, x2) -> x2);
+
+        // then
+        Assert.assertEquals(new BigDecimal("100.56789"), end);
+    }
+
+    @Test
+    public void testFive() {
+        // given
+        Stream<BigDecimal> start = Stream.of(
+                new BigDecimal("1"),
+                new BigDecimal("1.12345612341212351"),
+                new BigDecimal("10.142"),
+                new BigDecimal("-1.2"),
+                new BigDecimal("100.56789")
+        );
+
+        // when i get the average of these big decimals as doubles
+        // hint. use DoubleStream
+//        OptionalDouble avg = null;
+        OptionalDouble avg = start
+                .mapToDouble(BigDecimal::doubleValue)
+                .average();
+
+
+        // then
+        Assert.assertEquals(22.326669224682426, avg.getAsDouble(), 0.0);
+    }
+}
